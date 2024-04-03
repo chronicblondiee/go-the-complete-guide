afd8b844b591e1e5731a7a18444aea0134c94ed2
Aodhan Brown <chronicblondiee@gmail.com>
2024-04-04 00:25:25 +0100

    profit-cal done

---
+++ profit-calculator/go.mod
@@ -0,0 +1,3 @@
+module github.com/chronicblondiee/go-the-complete-guide/tree/profit-calculator
+
+go 1.22.2
--- profit-calculator/main.go
+++ profit-calculator/main.go
@@ -1,1 +1,38 @@
 package main
+
+import "fmt"
+
+/*
+Profit Calulator
+
+Take the following as user input
+
+revenue, expenses and tax rate
+
+Calculate the earings before tax (EBT)
+
+Calculate the earings after tax (EBT)
+
+output EBT, profit and the ratio
+*/
+
+func main() {
+	var revenue float64
+	var expenses float64
+	var taxRate float64
+
+	fmt.Println("Please Enter the following information")
+	fmt.Print("Total Revenue: ")
+	fmt.Scan(&revenue)
+	fmt.Print("\nTotal Expenses: ")
+	fmt.Scan(&expenses)
+	fmt.Print("\nYour Current Tax Rate in decimal e.g 10 or 37.5 : ")
+	fmt.Scan(&taxRate)
+
+	earingsBeforeTax := revenue - expenses
+	profit := earingsBeforeTax * (1 - taxRate/100)
+	ratio := earingsBeforeTax / profit
+	// use a printf to print them on the same line works the same as in c
+	fmt.Printf("EBT: %f Profit: %f Ratio: %f \n", earingsBeforeTax, profit, ratio)
+
+}
