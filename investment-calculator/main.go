ffcd531b4bc77d78273ab6669f6d69bfdcbee191
Aodhan Brown <chronicblondiee@gmail.com>
2024-04-03 23:22:46 +0100

    added const

--- investment-calculator/main.go
+++ investment-calculator/main.go
@@ -15,13 +15,21 @@ func main() {
 	// var years float64 = 10
 
 	// short hand for declaring vars that infers the type and multi vars assigments
-	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+	//investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+	const inflationRate = 7.5
+	investmentAmount := 1000.0
+	years := 10.0
+	expectedReturnRate := 5.5
+
+	// take user input
 
 	// you can cast your vars using the following syntax
 	// var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
 	// better way to write this would be to use the correct types at the start
-	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 	//fmt.Printf("%.2f \n", futureValue)
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 
 }
