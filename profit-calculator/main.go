f7d257a5abe14cf3e259d4763f12dba4fbdd1eee
Aodhan Brown <chronicblondiee@gmail.com>
2024-04-04 23:14:11 +0100

    covert profit calculator into functions

--- profit-calculator/main.go
+++ profit-calculator/main.go
@@ -17,10 +17,17 @@ output EBT, profit and the ratio of EBT and Profit
 */
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
 
+	revenue, expenses, taxRate := takeInput()
+
+	earingsBeforeTax, profit, ratio := doTaxCalculations(revenue, expenses, taxRate)
+	// use a printf to print them on the same line works the same as in c
+	fmt.Printf("EBT: %.2f Profit: %.2f Ratio: %.2f \n", earingsBeforeTax, profit, ratio)
+
+}
+
+func takeInput() (revenue float64, expenses float64, taxRate float64) {
+	// take user input
 	fmt.Println("Please Enter the following information")
 	fmt.Print("Total Revenue: ")
 	fmt.Scan(&revenue)
@@ -29,10 +36,14 @@ func main() {
 	fmt.Print("\nYour Current Tax Rate: ")
 	fmt.Scan(&taxRate)
 
-	earingsBeforeTax := revenue - expenses
-	profit := earingsBeforeTax * (1 - taxRate/100)
-	ratio := earingsBeforeTax / profit
-	// use a printf to print them on the same line works the same as in c
-	fmt.Printf("EBT: %f Profit: %f Ratio: %f \n", earingsBeforeTax, profit, ratio)
+	return revenue, expenses, taxRate
+}
 
+func doTaxCalculations(revenue float64, expenses float64, taxRate float64) (earingsBeforeTax float64, profit float64, ratio float64) {
+
+	earingsBeforeTax = revenue - expenses
+	profit = earingsBeforeTax * (1 - taxRate/100)
+	ratio = earingsBeforeTax / profit
+
+	return earingsBeforeTax, profit, ratio
 }
