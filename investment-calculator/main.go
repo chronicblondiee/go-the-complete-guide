57dd55fb1d7a4b71a821454d0c63246f8428e989
Aodhan Brown <chronicblondiee@gmail.com>
2024-04-04 22:40:34 +0100

    added functions

--- investment-calculator/main.go
+++ investment-calculator/main.go
@@ -7,6 +7,9 @@ import (
 	"math"
 )
 
+// move const to be global
+const inflationRate = 7.5 // you would prob just pass this in but for example of global scope constants where constants are fine to be global scope
+
 // main function
 func main() {
 
@@ -16,10 +19,14 @@ func main() {
 
 	// short hand for declaring vars that infers the type and multi vars assigments
 	//investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
-	const inflationRate = 7.5
-	investmentAmount := 0.0
-	years := 0.0
-	expectedReturnRate := 0.0
+
+	// investmentAmount := 0.0
+	// years := 0.0
+	// expectedReturnRate := 0.0
+	// can be empty as initlised to 0.0 by default for float
+	var investmentAmount float64
+	var years float64
+	var expectedReturnRate float64
 
 	// take user input
 	fmt.Print("Please input your Investment Amount: ")
@@ -32,9 +39,11 @@ func main() {
 	// you can cast your vars using the following syntax
 	// var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
 	// better way to write this would be to use the correct types at the start
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
-	// create a formatted string
+	//futureValue := investmentAmount * math.Pow(1+investmentAmount/100, years)
+	//futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	// same as above using a functions example
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, years, inflationRate)
+	// create a formatted string using sprintf
 	formattedFv := fmt.Sprintf("Future Value: %.2f \n", futureValue)
 	formattedRFV := fmt.Sprintf("Future Value (Adjusted for inflation) %.2f \n", futureRealValue)
 	// output formatted
@@ -43,5 +52,18 @@ func main() {
 	//fmt.Println("Future Value:", futureValue)
 	//fmt.Println(futureRealValue)
 	fmt.Print(formattedFv, formattedRFV)
+	// output using multiline printf
+	/*
+		fmt.Printf(`Future Value: %.2f
+		Future Value (Adjusted for inflation) %.2f`, futureValue, futureRealValue)
+	*/
+}
 
+// get future value function
+func calculateFutureValues(investmentAmount float64, years float64, inflationRate float64) (float64, float64) {
+	// keep in mind when returning values single returns only need type without () float64 multi returns uses (float64, int, string)
+	fv := investmentAmount * math.Pow(1+investmentAmount/100, years)
+	rfv := fv / math.Pow(1+inflationRate/100, years)
+
+	return fv, rfv
 }
